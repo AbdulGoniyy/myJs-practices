bf7141af5703a4d2919ccc9b030a59cb4a2bcf4a
Abdganiyy <kelani.abdganiyy1@gmail.com>
2024-02-06 09:01:41 +0100

    multil filing 2.0

---
+++ 5a.js
@@ -0,0 +1,17 @@
+var name = 'AbdulGoniyy';
+console.log(`My name is ${name}`);
+
+var cost = 5 + 3 + 9;
+
+console.log(`Cost of food: $${cost}`);
+
+var tax = cost*0.1;
+
+ var tax = 1.7;
+
+console.log(`tax (10%): $${tax}`);
+
+console.log(`${cost + tax}`);
+
+var totalCost = 18.7;
+console.log(`Total cost: ${totalCost}`);
---
+++ 6a-6b.js
@@ -0,0 +1,19 @@
+const hour =  14;
+
+
+if (hour >= 6 && hour <= 12) {
+  console.log('Good morning!');
+} else if (hour >= 13 && hour <= 17) {
+  console.log('Good afternoon!');
+}  else {
+  console.log('Good night!');
+}
+
+const name = 'ABDUL-GONIYY';
+if (hour >= 6 && hour <= 12) {
+  console.log(`Good morning ${name}!`);
+} else if (hour >= 13 && hour <= 17) {
+  console.log(`Good afternoon ${name}!`);
+}  else {
+  console.log(`Good night ${name}!`);
+}
